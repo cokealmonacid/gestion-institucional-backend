53c38eb5942db5e5f25a8b29a76d98ee9e7f505f
cokealmonacid <jorgealmonacid24@gmail.com>
2026-05-19 10:19:53 -0400

    Updated auth route prefix

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,13 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
 
-Route::prefix('v1')->controller(AuthController::class)->group(function(){
-    Route::post('login', 'login');
-    Route::post('logout', 'logout')->middleware('auth:sanctum');
+Route::prefix('v1')->group(function () {
 
-    Route::post('forget-password', 'sendResetLink');
-    Route::post('reset-password-confirm', 'resetPassword');
+    Route::prefix('auth')->controller(AuthController::class)->group(function () {
+        Route::post('login', 'login');       
+        Route::post('logout', 'logout')->middleware('auth:sanctum'); 
+        Route::post('forget-password', 'sendResetLink');     
+        Route::post('reset-password-confirm', 'resetPassword');
+    });
+
 });
